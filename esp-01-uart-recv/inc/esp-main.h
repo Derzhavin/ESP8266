c30d5bf2e94ee9b8f91d7bd9eb800abeb2d4bc44
Derzhavin <derzhavind@bk.ru>
2020-08-06 14:33:27 +0300

    uart recv

---
+++ esp-01-uart-recv/inc/esp-main.h
@@ -0,0 +1,13 @@
+/*
+ * esp-main.h
+ *
+ *  Created on: Jul 22, 2020
+ *      Author: data-scientist
+ */
+
+#ifndef INC_ESP_MAIN_H_
+#define INC_ESP_MAIN_H_
+
+
+
+#endif /* INC_ESP_MAIN_H_ */
---
+++ esp-01-uart-recv/inc/user_config.h
@@ -0,0 +1,13 @@
+#ifndef _USER_CONFIG_H_
+# define _USER_CONFIG_H_
+
+/* stupid autoinclude by SDK-includes */
+
+#endif /* _USER_CONFIG_H_ */
+
+/*
+  Local variables: ***
+  mode: c ***
+  c-basic-offset: 4 ***
+  End: ***
+*/
